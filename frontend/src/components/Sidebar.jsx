b86f25ca600d843a64639139f3698356c8795342
Waled Karim <waledkarim445@gmail.com>
2025-03-13 17:01:37 +0600

    sidebar image issue fixed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside className="h-full w-15 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
 
             <div className="border-b border-base-300 w-full p-5">
 
@@ -33,7 +33,7 @@ const Sidebar = () => {
                         <span className="font-medium hidden lg:block">Contacts</span>
                   </div>
 
-                  {/* TODO: Online filter toggle */}
+                  {/* Online filter toggle switch */}
                   <div className="mt-3 hidden lg:flex items-center gap-2">
 
                         <label className="cursor-pointer flex items-center gap-2">
@@ -52,6 +52,7 @@ const Sidebar = () => {
 
             </div>
 
+            {/* Users section */}
             <div className="overflow-y-auto w-full py-3">
 
             { 
@@ -68,14 +69,17 @@ const Sidebar = () => {
                                         <img
                                           src={user.profilePic || "/avatar.png"}
                                           alt={user.name}
-                                          className="size-12 object-cover rounded-full"/>
+                                          className="size-12 object-cover rounded-full"
+                                        />
 
-                                        {onlineUsers.includes(user._id) && (
-                                          <span
-                                            className="absolute bottom-0 right-0 size-3 bg-green-500 
-                                            rounded-full ring-2 ring-zinc-900"
-                                          />
-                                        )}
+                                        {
+                                          onlineUsers.includes(user._id) && (
+                                            <span
+                                              className="absolute bottom-0 right-0 size-3 bg-green-500 
+                                              rounded-full ring-2 ring-zinc-900"
+                                            />
+                                          )
+                                        }
 
                                 </div>
 
